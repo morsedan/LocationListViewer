db6268e9bbccd533395271b8cd669bfcff2eebb1
Dan Morse <djmthedev@gmail.com>
2020-02-20 15:58:34 -0800

    Added project

---
+++ LocationListViewer/LocationListViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationListViewer
+//
+//  Created by morse on 2/20/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
