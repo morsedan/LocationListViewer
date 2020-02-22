7fbd7ad8c98da3e1522f7f05a89060a5e9eaf19d
Dan Morse <djmthedev@gmail.com>
2020-02-21 16:50:16 -0800

    added NetworkController file and got API key populating to info.plist without it being tracked by GitHub.

---
+++ LocationListViewer/LocationListViewer/NetworkController.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkController.swift
+//  LocationListViewer
+//
+//  Created by morse on 2/21/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+    
+}
